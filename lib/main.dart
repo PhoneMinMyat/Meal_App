777f97b4a053209cccb958dfade75401e5c88dd3
PhoneMinMyat <phoneminmyat1999@gmail.com>
2021-11-27 00:39:10 +0630

    create project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light().copyWith(primary: Colors.blue),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal App'),
+      ),
+      body: const Center(
+        child: Text('Meal App'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+  const CategoryItem({required this.category, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
